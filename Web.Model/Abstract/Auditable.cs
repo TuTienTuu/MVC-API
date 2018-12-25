04121bb9f644f75ab39f6715bd0279f9ac471859
TuuTuTien <tutientuu140596@gmail.com>
2018-12-25 16:17:49 +0700

    the second commit

---
+++ Web.Model/Abstract/Auditable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { get; set; }
+        [MaxLength(255)]
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        [MaxLength(255)]
+        public string UpdatedBy { get; set; }
+
+        [MaxLength(255)]
+        public string MetaKeyword { get; set; }
+
+        [MaxLength(255)]
+        public string MetaDescription { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
